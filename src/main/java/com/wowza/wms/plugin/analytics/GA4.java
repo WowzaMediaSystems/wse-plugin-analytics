4679681845abbf3e15ae80117f667099a56daf2a
Roger Littin <roger@wowza.com>
2023-06-30 13:54:57 +0100

    Update Copyright comments in code.

--- src/main/java/com/wowza/wms/plugin/analytics/GA4.java
+++ src/main/java/com/wowza/wms/plugin/analytics/GA4.java
@@ -1,3 +1,7 @@
+/*
+ * This code and all components (c) Copyright 2006 - 2023, Wowza Media Systems, LLC.  All rights reserved.
+ * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ */
 package com.wowza.wms.plugin.analytics;
 
 import com.wowza.wms.application.IApplicationInstance;
